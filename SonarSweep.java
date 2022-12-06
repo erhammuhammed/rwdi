3abbbe7c808e615db72ba7174ebcd3922a006c71
erhammuhammed <84924804+erhammuhammed@users.noreply.github.com>
2022-12-06 13:13:34 -0500

    Day 1. 2021 Solution

---
+++ SonarSweep.java
@@ -0,0 +1,27 @@
+package org.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SonarSweep {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		File file = new File("E:\\Work\\STS-Workspace\\Assessment_RWDI\\src\\org\\test\\Data1");
+		int count = 0;
+		int linecount = 1;
+		Scanner myReader = new Scanner(file);
+		int initial = Integer.parseInt(myReader.nextLine().trim());
+		while (myReader.hasNextLine()) {
+			linecount++;
+			String current = myReader.nextLine().trim();
+			if (initial < Integer.parseInt(current)) {
+				count++;
+			}
+			initial = Integer.parseInt(current);
+		}
+		System.out.println("Count:" + count+" lines:"+linecount);
+	}
+
+}
